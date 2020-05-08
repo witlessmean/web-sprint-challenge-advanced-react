7341ac9b1763cedd26d2af7a0296b0dbc7dc66a1
Shawn Elston <elstonshawn1@yahoo.com>
2020-05-08 10:47:38 -0700

    mvp

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
+configure({ adapter: new Adapter() });
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+// test("form header renders", () => {});
 
-test("form shows success message on submit with form details", () => {});
+// test("form shows success message on submit with form details", () => {});
+
+describe("Header tests", () => {
+  it("Header should render to the user", () => {
+    const wrapper = shallow(<CheckoutForm />);
+    expect(wrapper.find("nav.container"));
+  });
+});
+
+describe("Form button tests", () => {
+  it("form shows success message on submit with form details", () => {
+    const wrapper = shallow(<CheckoutForm />);
+    expect(
+      wrapper.find("form button").simulate("click").find("div.success-message")
+    );
+  });
+});
--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+ constructor(props) {
+   super(props)
 
+   this.state = {
+     plants: [],
+   }
+}
+
+componentDidMount() {
+  axios.get('http://localhost:3333/plants')
+  .then((response) => {
+    console.log(response);
+    this.setState({
+      plants: response.data.plantsData
+    })
+  }).catch(error => {
+    console.log (error);
+  })
+}
+ 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
